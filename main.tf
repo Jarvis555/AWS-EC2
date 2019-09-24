3edfdd921a99c412d5a130ac40d0a9b3e35658de
Jarvis555 <jarvisjackson591@gmail.com>
2019-09-24 07:30:40 -0500

    second commit

--- main.tf
+++ main.tf
@@ -16,3 +16,18 @@ resource "aws_key_pair" "tf_auth" {
   public_key = "${file(var.public_key_path)}"
 }
 
+resource "aws_instance" "tf_server" {
+  count         = "${var.instance_count}"
+  instance_type = "${var.instance_type}"
+  ami           = "${data.aws_ami.server_ami.id}"
+
+  tags {
+    Name = "tf_server-${count.index +1}"
+  }
+
+  key_name               = "${aws_key_pair.tf_auth.id}"
+  vpc_security_group_ids = ["${var.security_group}"]
+  subnet_id              = "${element(var.subnets, count.index)}"
+  
+}
+
--- variables.tf
+++ variables.tf
@@ -5,3 +5,13 @@ variable "key_name" {}
 
 variable "public_key_path" {}
 
+variable "instance_count" {}
+
+variable "instance_type" {}
+
+variable "security_group" {}
+
+variable "subnets" {
+  type = "list"
+}
+
